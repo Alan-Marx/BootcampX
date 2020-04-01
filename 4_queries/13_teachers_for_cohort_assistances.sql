b7a8d27352e6fb200b13db4daecedcc6e347f9b8
Alan Marx <alanmarx@hotmail.com>
2020-04-01 00:53:11 +0000

    deleted unecessary comments

--- 4_queries/13_teachers_for_cohort_assistances.sql
+++ 4_queries/13_teachers_for_cohort_assistances.sql
@@ -6,15 +6,3 @@ JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'JUL02'
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-
-
-
-
-
--- SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
--- FROM teachers
--- JOIN assistance_requests ON teachers.id = teacher_id
--- JOIN students ON students.id = student_id
--- JOIN cohorts ON cohorts.id = cohort_id
--- WHERE cohorts.name = 'JUL02'
--- ORDER BY teacher;
