404887e687f00b6c5b1cc5722f01e3d76d3b8dc2
Alan Marx <alanmarx@hotmail.com>
2020-04-01 00:55:42 +0000

    standardized formatting

--- 4_queries/13_teachers_for_cohort_assistances.sql
+++ 4_queries/13_teachers_for_cohort_assistances.sql
@@ -1,4 +1,4 @@
-SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) as total_assistances
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
